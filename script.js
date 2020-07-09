9e2107681a1b7f27e455dd6073d5e73440c9461c
Mike Hayes <mikehayes330@gmail.com>
2020-07-08 22:59:38 -0400

    stuck on local storage

--- script.js
+++ script.js
@@ -1,109 +1,112 @@
-
-
-
 // var currentCity = ""
 var APIKey = "64c34ee7c70a13599aaf08ac59e6b5b5";
+var recentCities = [];
 
-$('button').on('click', test)
-
-
-
+$("#search").on("click", getSearchedCity);
 
 
 
 
 
-function test(event){
-    event.preventDefault();
+
+
+
+
+function getSearchedCity(event) {
+  event.preventDefault();
+
+  
+  var searchedCity = $("#searchTerm").val();
+  recentCities.push(searchedCity);
+  
+  localStorage.setItem("recentSearches", JSON.stringify(recentCities) )
+  
+  var currentLat, currentLon;
+  saveCities();
     
-    var recentCities = [];
-    var searchedCity = $('#searchTerm').val();
+  
+
+  
+
+  
+
+  var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + searchedCity + "&appid=" + APIKey;
+  
+
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then(function (response) {
+    var currentTempF = (response.main.temp - 273.15) * 1.8 + 32;
+    var currentHumidity = response.main.humidity;
+    var currentWindSpeedMPH = response.wind.speed * 2.237;
+    var currentCity = response.name;
+    var newDiv = $("<div>");
+    var p = $("<p>");
+    var searchedCityElement = $("#searchedCities");
+    var currentCityElement = $(".currentCity");
+    var currentTempElement = $(".currentTemp");
+    var currentWindElement = $(".currentWind");
+    var day1Element = $('#day1');
+    var currentLon = response.coord.lon
+    var currentLat = response.coord.lat
     
-    saveCities();
-    
-    var queryURL = 'http://api.openweathermap.org/data/2.5/weather?q='+ searchedCity + '&appid=' + APIKey;
-    var forcastQueryURL = 'http://api.openweathermap.org/data/2.5/forcast?q='+ searchedCity + '&appid=' + APIKey;
-
-    console.log(forcastQueryURL);
-
+    searchedCityElement.text("Current City: " + currentCity);
+    currentCityElement.html("<h2> Current City: " + currentCity + " (" + moment().format("LL") + ") </h2>");
+    currentTempElement.html("<h2> Current Tempature: " + Math.round(currentTempF) + " F</h2>");
+    currentWindElement.html("<h2> Current Wind Speed: " + Math.round(currentWindSpeedMPH) + " MPH</h2>");
+    day1Element.html("fdsa")
 
     
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-      })
-        .then(function(response) {
-          
-        var currentTempF = (response.main.temp - 273.15) * 1.80 + 32;
-        var currentHumidity = response.main.humidity;
-        var currentWindSpeedMPH = (response.wind.speed) * 2.237;
-        var currentCity = response.name
-        var newDiv = $('<div>');
-        var p = $('<p>');
-        var searchedCityElement = $('#searchedCities');
-        var currentCityElement = $('.currentCity');
-        var currentTempElement = $('.currentTemp');
-        var currentWindElement = $('.currentWind')
-       
 
+  
+  });
 
-          console.log(response)
-          console.log("Current Temp: " + Math.round(currentTempF) + " F");
-          console.log("Current City: " + currentCity)
-          console.log("Current Wind Speed: " + Math.round(currentWindSpeedMPH) + " MPH")
-           searchedCityElement.text("Current City: " + currentCity)
-           currentCityElement.html("<h2> Current City: " + currentCity + " (" + moment().format('LL') + ") </h2>")
-           currentTempElement.html("<h2> Current Tempature: " + Math.round(currentTempF) + " F</h2>")
-           currentWindElement.html("<h2> Current Wind Speed: " + Math.round(currentWindSpeedMPH) + " MPH</h2>")
-          
-          
-          
-          
-          
-          //  newDiv.text("Current City: " + currentCity);
-          
-          //$('.weatherDiv').append(newDiv);
-         // newDiv.text("Current Temp: " + Math.round(currentTempF) + " F");
-          
-          
-         // $('.weatherDiv').append(newDiv);
-          
-          
-           
+  
+  // var forecastQueryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + currentLat + "&lon=" + currentLon + "&exclude=minutely,hourly&appid=" + APIKey;
 
-        //   for (var i = 0; i < results.length; i++) {
-        //     var gifDiv = $("<div>");
+   
 
-        //     var rating = results[i].rating;
+  // $.ajax({
+  //   url: forecastQueryURL,
+  //   method: "GET",
+  // }).then(function (forecast) {
+    
+  //   console.log(forecast)
+  //   var day1Element = $('#day1');
 
-        //     var p = $("<p>").text("Rating: " + rating);
-
-        //     var personImage = $("<img>");
-        //     personImage.attr("src", results[i].images.fixed_height.url);
-
-        //     gifDiv.prepend(p);
-        //     gifDiv.prepend(personImage);
-
-        //     $("#gifs-appear-here").prepend(gifDiv);
-         // }
-        });
-        function saveCities(){
-          recentCities.push(searchedCity);
-          localStorage.setItem("recentCities", recentCities)
-        }
     
     
+  //   day1Element.html("fdsa")
+
     
+
+  
+  // });
+  
+  
+  
+  
+  
+  function saveCities() {
+   console.log(searchedCity);
+   console.log(recentCities);
+  
     console.log(recentCities)
     
     
+    // localStorage.setItem("recentCities", recentCities);
+    // console.log(recentCities)
+  }
+
+  function showRecentCities(){
+  
+  console.log(localStorage.getItem(recentCities))
+    //  recentList = $('<ul>')
+  //   searchedCityElement.append(recentSearchedCities)
     
-    
-    console.log(queryURL)
+  }
+  
+
+  
 }
-
-
-
-
-
-
